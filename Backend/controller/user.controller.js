94737572f36491a56f7d30457ccf4f3d71610c9b
chmueedzafar <chmueedzafar@gmail.com>
2024-12-19 01:49:12 +0500

    data base connect commit

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,3 +1,26 @@
-export const register = (req, res) => {
-    console.log("hello i am register user")
-}
+import { User } from "../models/user.model.js"
+
+export const register = async (req, res) => {
+    const {name, email, password, phone, photo, education, role} = req.body
+    if(!name || !email || !password || !phone || !education || !role){
+        return res.status(400).json({
+            success: false,
+            message: "Please fill required fields"
+        })
+    }
+    const user =User.findOne({email})
+    if(user){
+        return res.status(400).json({
+            success: false,
+            message: "User already exists with this email"
+        })
+    }
+    const newUser = new User({name, email, password, phone, photo, education, role});
+   await newUser.save()
+   if(newUser){
+    return res.status(201).json({
+        message: "User Registered successfully"
+    })
+   }
+    
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import userRoutes from './routes/user.route.js'
 const app = express()
 dotenv.config()
 
 const port = process.env.PORT
 const mongoURI = process.env.MONGO_URI
+// middleware
+app.use(express.json())
+
 // db connection
 try{
 mongoose.connect(mongoURI)
@@ -13,6 +17,8 @@ console.log("Connected to MongoDB")
 }catch(error){
     console.log(error)
 }
+// define routes
+app.use('/api/user', userRoutes)
 
 
 app.listen(port, () => {
